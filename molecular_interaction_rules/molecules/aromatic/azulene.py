e04d9a4b372e13653dcc5c15aba05416dc490dfd
Sul <sharifsuliman1@gmail.com>
2024-11-18 09:04:37 -0600

    Update azulene.py

--- molecular_interaction_rules/molecules/aromatic/azulene.py
+++ molecular_interaction_rules/molecules/aromatic/azulene.py
@@ -52,6 +52,8 @@ class Azulene(object):
     ATOM H7     HQR7A     0.092                                     ! Penalty =  5.357
     ATOM H8     HQR7A     0.094                                     ! Penalty =  3.525
 
+    Rule 1,2
+
     '''
 
     # Covered
@@ -88,7 +90,7 @@ class Azulene(object):
     def get_monomer_b_species(self):
 
         monomer_b_species = {
-            'H1': self.monomer_b_shaped_hydrogen_zmatrix_carbonyl(),
+            'H1': self.monomer_b_shaped_hydrogen_zmatrix(),
             'RC1': self.monomer_b_pi_stack_zmatrix()
         }
 
@@ -130,7 +132,7 @@ class Azulene(object):
         zmatrix = '''\
           X11
           C11   X11  1.4000
-          C12   C11  1.4090   X11   64.1800
+          C12   C11  1.4090   X11   64.2850
           C13   C12  1.4090   C11  128.9389    X11    0.0000
           C14   C13  1.4090   C12  128.9389    C11   -0.0000
           C15   C14  1.4090   C13  125.6525    C12 -180.0000
@@ -156,44 +158,17 @@ class Azulene(object):
 
         return textwrap.dedent(zmatrix), atom_name
 
-    def monomer_b_shaped_hydrogen_zmatrix_oxygen(self):
+    def monomer_b_shaped_hydrogen_zmatrix(self):
 
-        zmatrix = '''\
-            H21    :1  mm  :2   135.0000    :3   180.0000
-            X21   H21  1.0000    :1   90.0000     :2    0.0000
-            C21   H21  1.0756   X21   90.0000     :1  180.0000
-            C22   C21  1.3774   H21  120.0000     :1  180.0000
-            C23   C22  1.4090   C21  128.9389    H21  180.0000
-            C24   C23  1.4090   C22  128.9389    C21   -0.0000
-            C25   C24  1.4090   C23  125.6525    C22 -180.0000
-            C26   C25  1.4090   C24  108.3817    C23 -180.0000
-            H22   C25  1.0921   C24  125.6525    C23    0.0000
-            C27   C26  1.4090   C25  109.9255    H22  -180.0000
-            C28   C23  2.6128   C22  101.3243    C21   -0.0000
-            C29   C28  1.4090   C24  127.6958    C23   -0.0000
-            C30   C21  1.4090   C22  129.6993    C23   -0.0000
-            H23   C26  1.0936   C25  125.6525    C24 -180.0000
-            H24   C27  1.0936   C26  126.5100    C25 -180.0000
-            H25   C29  1.0936   C28  115.5882    C24 -180.0000
-            H26   C30  1.0936   C21  115.5882    C22 -180.0000
-            H27   C23  1.0936   C22  116.0586    C21 -180.0000
-            H28   C22  1.0936   C23  115.5882    C24 -180.0000
-            0 1
+        '''
+        For replusion the angle is 180, and the dihedral is 180
         '''
 
-        atom_name = [
-          'H5', 'C7', 'C6', 'C5', 'C4', 'C3', 'C2', 'H3', 'C1', 'C10', 'C9', 'C8', 'H2', 'H1', 'H9', 'H8', 'H7', 'H6'
-        ],
-
-        return textwrap.dedent(zmatrix), atom_name
-
-    def monomer_b_shaped_hydrogen_zmatrix_carbonyl(self):
-
         zmatrix = '''\
-                H21   X11  DISTANCE  :2   90.0000    :3   90.0000
-                X21   H21  1.0000    :1   90.0000     :2    0.0000
+                H21    :1  DISTANCE  :2   ANGLE    :3   90.0000
+                X21   H21  1.0000    :1   90.0000     :2   DIHEDRAL
                 C21   H21  1.0756   X21   90.0000     :1  180.0000
-                C22   C21  1.3774   H21  120.0000     :1  180.0000
+                C22   C21  1.3774   H21  120.0000    X21    0.0000
                 C23   C22  1.4090   C21  128.9389    H21  180.0000
                 C24   C23  1.4090   C22  128.9389    C21   -0.0000
                 C25   C24  1.4090   C23  125.6525    C22 -180.0000
@@ -221,10 +196,10 @@ class Azulene(object):
     def monomer_b_pi_stack_zmatrix(self):
 
         zmatrix = '''\
-            X21    :1  DISTANCE  :2   180.0000     :3   180.0000
+            X21    :1  DISTANCE  :2   ANGLE     :3   90.0000
             C21   X21  1.3940    :1   90.0000     :2  180.0000
             C22   C21  1.3774   X21   60.0000     :1   90.0000
-            C23   C22  1.3774   C21  120.0000    X21    0.0000
+            C23   C22  1.3774   C21  -120.0000   X21   DIHEDRAL
             C24   C23  1.4090   C22  128.9389    C21   -0.0000
             C25   C24  1.4090   C23  125.6525    C22 -180.0000
             C26   C25  1.4090   C24  108.3817    C23 -180.0000
@@ -248,3 +223,4 @@ class Azulene(object):
         ],
 
         return textwrap.dedent(zmatrix), atom_name
+
