e21745bffa7b0181289048972b310942a0be6e1f
Sul <sharifsuliman1@gmail.com>
2024-11-18 09:08:42 -0600

    Update imidazole.py

--- molecular_interaction_rules/molecules/aromatic/imidazole.py
+++ molecular_interaction_rules/molecules/aromatic/imidazole.py
@@ -9,6 +9,63 @@ import textwrap
 
 class Imidazole(object):
 
+    '''
+
+    CGenFF
+
+    GROUP
+    ATOM CG   CG2R51  -0.05 !       HD1    HE1
+    ATOM HG   HGR52    0.09 !        |     /
+    ATOM CD2  CG2R51   0.22 !       ND1--CE1
+    ATOM HD2  HGR52    0.10 !      /     ||
+    ATOM ND1  NG2R51  -0.36 ! HG-CG      ||
+    ATOM HD1  HGP1     0.32 !     \\     ||
+    ATOM CE1  CG2R53   0.25 !      CD2--NE2
+    ATOM HE1  HGR52    0.13 !       |
+    ATOM NE2  NG2R50  -0.70 !      HD2
+
+    DGenFF
+
+    RESI IMID          0.000 ! Imidazole
+    !
+    !       HD1    HE1
+    !        |     /
+    !       ND1--CE1
+    !      /     ||
+    ! HG-CG      ||
+    !     \\     ||
+    !      CD2--NE2
+    !       |      :
+    !      HD2
+    !
+
+    GROUP
+    ATOM CG   CD2R5A  -0.343  ALPHA -1.632  THOLE 0.949
+    ATOM HG   HDR5A    0.215
+    ATOM CD2  CD2R5A   0.037  ALPHA -1.626  THOLE 0.949
+    ATOM HD2  HDR5A    0.091
+    ATOM ND1  ND2R5A  -0.053  ALPHA -1.393  THOLE 0.670
+    ATOM HD1  HDP1A    0.244
+    ATOM CE1  CD2R5B   0.040  ALPHA -1.632  THOLE 1.568
+    ATOM HE1  HDR5B    0.107
+    ATOM NE2  ND2R5B   0.000  ALPHA -1.044  THOLE 0.788
+    ATOM LP1  LPD     -0.338
+
+    Rule 2, 3
+    '''
+
+    # Covered
+
+    __CGENFF_ATOM_TYPES__ = {
+      'RC1': ['CG2R51', 'NG2R51', 'CG2R53', 'NG2R51'],
+      'N1': ['ND2R5B']
+    }
+
+    __DGENFF_ATOM_TYPES__ = {
+      'RC1': ['CD2R5A', 'CD2R5B', 'ND2R5B', 'ND2R5A'],
+      'N1': ['NG2R50']
+    }
+
     def __init__(self):
 
         self.resi_name = 'IMID'
@@ -83,13 +140,11 @@ class Imidazole(object):
 
     def monomer_b_nitrogen_zmatrix(self):
 
-        '''dont work'''
-
         zmatrix = '''\
-            N21   :1  DISTANCE  :2  180.0000    :3   180.0000
-            X21  N21  1.0000   :1   135.0000    :2    0.0000
-            C22  N21  1.3730  X21   90.0000    :1  180.0000
-            C21  C22  1.3918  N21  110.7737    :1  180.0000
+            N21   :1  DISTANCE  :2  ANGLE      :3   DIHEDRAL
+            C22  N21  1.3730    :1  126.5480   :2     0.0000
+            C23  N21  1.3730    :1  126.9043   :2   180.0000
+            C21  C22  1.3918  N21  110.7737   C23    0.0000
             C23  N21  1.3351  C22  105.1514   C21   -0.0000
             N22  C21  1.3815  C22  104.8582   N21    0.0000
             H21  C23  1.0872  N21  126.0493   C22 -180.0000
@@ -109,9 +164,9 @@ class Imidazole(object):
     def monomer_b_aromatic_zmatrix(self):
 
       zmatrix = '''\
-            X21   :1  DISTANCE  :2   180.0000  :3   90.0000
-            C21  X21  1.1134    :1   90.0000   :2  180.0000
-            C22  C21  1.3918  X21   60.0000    :1   90.0000
+            X21   :1  DISTANCE  :2   ANGLE  :3   90.0000
+            C21  X21  1.1134    :1   90.0000   :2   0.0000
+            C22  C21  1.3918  X21   60.0000    :1  DIHEDRAL
             N21  C22  1.3730  C21  110.7737   X21   0.0000
             C23  N21  1.3351  C22  105.1514   C21   -0.0000
             N22  C21  1.3815  C22  104.8582   N21    0.0000
@@ -127,3 +182,4 @@ class Imidazole(object):
       ]
 
       return textwrap.dedent(zmatrix), atom_name
+
