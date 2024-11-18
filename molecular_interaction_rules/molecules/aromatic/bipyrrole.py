bdc3098ebb7fd613646e99bcee636e06bd1163f0
Sul <sharifsuliman1@gmail.com>
2024-11-18 09:10:23 -0600

    Update bipyrrole.py

--- molecular_interaction_rules/molecules/aromatic/bipyrrole.py
+++ molecular_interaction_rules/molecules/aromatic/bipyrrole.py
@@ -10,67 +10,6 @@ import textwrap
 
 class Bipyrrole(object):
 
-    '''
-
-    RESI 13BPO        0.00 ! C8H8N2, 1,3-bipyrrole, lf
-
-    GROUP
-    ATOM N1     NG2R57 -0.35
-    ATOM C2     CG2R51 -0.04
-    ATOM H2     HGR52   0.14
-    ATOM C3     CG2R51 -0.25
-    ATOM H3     HGR51   0.15
-    ATOM C4     CG2R51 -0.25 ! H4     H5    H9      H10
-    ATOM H4     HGR51   0.15 !  \     /      \      /
-    ATOM C5     CG2R51 -0.04 !   C4==C5       C9==C10
-    ATOM H5     HGR52   0.14 !   |     \     /     |
-    ATOM N6     NG2R51 -0.35 !   |      N1--C8     |
-    ATOM H6     HGP1    0.35 !   |     /     \     |
-    ATOM C7     CG2R51 -0.04 !   C3==C2       C7--N6
-    ATOM H7     HGR52   0.14 !  /     \      /      \
-    ATOM C8     CG2R57  0.25 ! H3     H2    H7      H6
-    ATOM C9     CG2R51 -0.25
-    ATOM H9     HGR51   0.15
-    ATOM C10    CG2R51 -0.04
-    ATOM H10    HGR52   0.14
-
-    RESI  ACSF      0.0000
-    GROUP
-    ATOM C1     CQ2R5A   -0.114   ALPHA   -1.5686  THOLE   1.0929   ! Penalty =  1.800
-    ATOM C2     CQ2R5A   -0.217   ALPHA   -1.7779  THOLE   1.1203   ! Penalty =  0.708
-    ATOM C3     CQ2R5A   -0.217   ALPHA   -1.7779  THOLE   1.1203   ! Penalty =  0.708
-    ATOM C4     CQ2R5A   -0.115   ALPHA   -1.5686  THOLE   1.0929   ! Penalty =  1.800
-    ATOM N1     NQ2R5H    0.103   ALPHA   -1.6699  THOLE   1.0549   ! Penalty = 10.896
-    ATOM C5     CQ2R5G    0.041   ALPHA   -1.7648  THOLE   1.2355   ! Penalty =  2.144
-    ATOM C6     CQ2R5A   -0.157   ALPHA   -1.5330  THOLE   1.1709   ! Penalty =  0.591
-    ATOM N2     NQ2R5A   -0.077   ALPHA   -1.6228  THOLE   1.0473   ! Penalty =  3.201
-    ATOM C7     CQ2R5A   -0.222   ALPHA   -1.7127  THOLE   1.0790   ! Penalty =  0.439
-    ATOM C8     CQ2R5A   -0.148   ALPHA   -1.6922  THOLE   1.1562   ! Penalty =  0.591
-    ATOM H1     HQP1A     0.227                                     ! Penalty =  0.932
-    ATOM H2     HQR5A     0.119                                     ! Penalty =  2.386
-    ATOM H3     HQP1C     0.128                                     ! Penalty =  0.613
-    ATOM H4     HQP1C     0.128                                     ! Penalty =  0.613
-    ATOM H5     HQR5A     0.119                                     ! Penalty =  2.386
-    ATOM H6     HQR5A     0.127                                     ! Penalty =  5.920
-    ATOM H7     HQR5A     0.163                                     ! Penalty =  0.933
-    ATOM H8     HQP1C     0.112                                     ! Penalty =  0.563
-
-    Rule 1,2
-
-    '''
-
-    # Covered
-
-    __CGENFF_ATOM_TYPES__ = {
-      'RC1': ['NG2R51', 'CG2R51', 'CG2R57'],
-      'H1': ['HGP1', 'NG2R57']
-    }
-
-    __DGENFF_ATOM_TYPES__ = {
-      'RC1': ['NQ2R5A', 'CQ2R5A', 'CQ2R5G'],
-      'H1': ['HQP1A', 'NQ2R5H']
-    }
-
     def __init__(self):
 
         self.resi_name = '13BPO'
