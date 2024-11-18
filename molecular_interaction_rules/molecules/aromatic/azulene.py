57df2c1767c47c0ff88d25ae2cdd70f54b594841
Sul <sharifsuliman1@gmail.com>
2024-11-18 09:09:50 -0600

    Update azulene.py

--- molecular_interaction_rules/molecules/aromatic/azulene.py
+++ molecular_interaction_rules/molecules/aromatic/azulene.py
@@ -9,65 +9,6 @@ import textwrap
 
 class Azulene(object):
 
-    '''
-
-    GROUP
-    ATOM C1   CG2R51  -0.30
-    ATOM H1   HGR51    0.20
-    ATOM C2   CG2R51  -0.17
-    ATOM H2   HGR51    0.20
-    ATOM C3   CG2R51  -0.30 !                 H5
-    ATOM H3   HGR51    0.20 !        H3       |       H6
-    ATOM C4   CG2RC7   0.07 !        |        C5-_   /
-    ATOM C5   CG2R71  -0.24 !        C3-_   //    -C6
-    ATOM H5   HGR71    0.23 !      //    -C4         \\
-    ATOM C6   CG2R71  -0.15 ! H2--C2       |          C7--H7
-    ATOM H6   HGR71    0.18 !       \   _=C10        /
-    ATOM C7   CG2R71  -0.22 !        C1=-    \   _=C8
-    ATOM H7   HGR71    0.21 !        |        C9=-   \
-    ATOM C8   CG2R71  -0.15 !        H1       |       H8
-    ATOM H8   HGR71    0.18 !                 H9
-    ATOM C9   CG2R71  -0.24
-    ATOM H9   HGR71    0.23
-    ATOM C10  CG2RC7   0.07
-
-    RESI  CT1000      0.0000
-    GROUP
-    ATOM C1     CQ2R7B    0.152   ALPHA   -2.3777  THOLE   1.1483   ! Penalty = 58.809
-    ATOM C2     CQ2R7B    0.152   ALPHA   -2.3777  THOLE   1.1483   ! Penalty = 58.809
-    ATOM C3     CQ2R5A   -0.261   ALPHA   -2.1409  THOLE   1.2709   ! Penalty = 33.336
-    ATOM C4     CQ2R7A   -0.110   ALPHA   -2.1421  THOLE   1.2194   ! Penalty = 28.162
-    ATOM C5     CQ2R5A   -0.261   ALPHA   -2.1409  THOLE   1.2709   ! Penalty = 33.336
-    ATOM C6     CQ2R7A   -0.110   ALPHA   -2.1421  THOLE   1.2194   ! Penalty = 28.162
-    ATOM C7     CQ2R5A   -0.182   ALPHA   -2.2335  THOLE   1.3610   ! Penalty =  8.399
-    ATOM C8     CQ2R7A   -0.072   ALPHA   -2.0114  THOLE   1.2991   ! Penalty =  7.665
-    ATOM C9     CQ2R7A   -0.072   ALPHA   -2.0114  THOLE   1.2991   ! Penalty =  7.665
-    ATOM C10    CQ2R7A   -0.079   ALPHA   -1.9300  THOLE   1.3599   ! Penalty =  4.228
-    ATOM H1     HQP1C     0.131                                     ! Penalty = 45.009
-    ATOM H2     HQR7A     0.091                                     ! Penalty = 18.712
-    ATOM H3     HQP1C     0.131                                     ! Penalty = 45.009
-    ATOM H4     HQR7A     0.091                                     ! Penalty = 18.712
-    ATOM H5     HQP1C     0.121                                     ! Penalty =  7.511
-    ATOM H6     HQR7A     0.092                                     ! Penalty =  5.357
-    ATOM H7     HQR7A     0.092                                     ! Penalty =  5.357
-    ATOM H8     HQR7A     0.094                                     ! Penalty =  3.525
-
-    Rule 1,2
-
-    '''
-
-    # Covered
-
-    __CGENFF_ATOM_TYPES__ = {
-      'RC1': ['CG2RC7', 'CG2R71'],
-      'H1': ['HGR71']
-    }
-
-    __DGENFF_ATOM_TYPES__ = {
-        'RC1': ['CQ2R7B', 'CQ2R7A'],
-        'H1': ['HQR7A']
-    }
-
     def __init__(self):
 
         self.resi_name = 'azul'
