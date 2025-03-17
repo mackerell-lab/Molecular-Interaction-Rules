23408e4b07243d4878ef3e4e476d4c3c88dfa78a
Sul <sharifsuliman1@gmail.com>
2025-03-17 10:18:27 -0500

    Update dithiolane.py

--- molecular_interaction_rules/molecules/non_aromatic/organosulfur/dithiolane.py
+++ molecular_interaction_rules/molecules/non_aromatic/organosulfur/dithiolane.py
@@ -1,1 +1,55 @@
+#!/usr/bin/env python3
+#
+# Lennard-Jones-Drill-2: Dithiolane
+# ---------------------------------
 
+# Imports
+# -------
+
+import textwrap
+
+class Dithiolane(object):
+
+    def __init__(self):
+
+        self.resi_name = 'MESH'
+
+    def get_monomer_a_species(self):
+
+        '''
+
+        Get the Monomer A Species
+
+        '''
+
+        monomer_a_species = {
+        }
+
+        return monomer_a_species
+
+    def get_monomer_a(self):
+
+        zmatrix = '''\
+        '''
+
+        atom_name = [
+        ]
+
+        return textwrap.dedent(zmatrix), atom_name
+
+    def get_monomer_b_species(self):
+
+        monomer_b_species = {
+        }
+
+        return monomer_b_species
+
+    def get_monomer_b(self):
+
+        zmatrix = '''\
+        '''
+
+        atom_name = [
+        ]
+
+        return textwrap.dedent(zmatrix), atom_name
